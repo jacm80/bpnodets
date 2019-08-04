16a9e480bb0b536acebf79e358d14634c03e42c3
Juan Canepa <juan.canepa>
2019-08-04 17:36:40 -0400

    fix: set all environment

--- src/tests/UserController.test.ts
+++ src/tests/UserController.test.ts
@@ -8,19 +8,37 @@ let _TOKEN_BEARER = null;
 const credentials = { email: 'montenegrogerson@gmail.com', password: '123456' };
 
 describe('bpnodets Backend test controller UserController', () => {
-    it('it should GET access_token', (done) => {
-    chai.request(app)
-        .post('/api/v1/authenticate')
-        .set('Content-Type', 'application/json')
-        .set('Accept', 'application/json')
-        .send(credentials)
-        .end((err, res) => {
-            console.log(res.body)
-            res.should.have.status(200);
-            res.body.success.should.be.eql(true);
-            _TOKEN_BEARER = res.body.access_token;
-            done();
-        });
+    it('it should GET token', (done) => {
+        chai.request(app)
+            .post('/api/v1/authenticate')
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .send(credentials)
+            .end((err, res) => {
+                console.log(res.body)
+                res.should.have.status(200);
+                res.body.success.should.be.eql(true);
+                _TOKEN_BEARER = res.body.token;
+                done();
+            });
+    });
+    it('it should GET all users', (done) => {
+        chai.request(app)
+            .get('/api/v1/users')
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .set('authorization', _TOKEN_BEARER)
+            .query({ page: 0, itemsPerPage: 10 })
+            .send(credentials)
+            .end((err, res) => {
+                console.log(res.body)
+                res.should.have.status(200);
+                res.body.success.should.be.eql(true);
+                res.body.should.have.property('data');
+                res.body.should.be.a('object');
+                res.body.data.should.be.a('array');
+                done();
+            });
     });
 });
 
