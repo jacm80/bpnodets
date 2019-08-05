0c756d302406980955a262f6845072979c67dcee
Juan Canepa <juan.canepa>
2019-08-04 20:29:25 -0400

    feat: added test update, delete UserController.test.ts

--- src/tests/UserController.test.ts
+++ src/tests/UserController.test.ts
@@ -5,6 +5,7 @@ import chaiHttp = require('chai-http');
 const should = chai.should();
 chai.use(chaiHttp);
 let _TOKEN_BEARER = null;
+let _ID_USER = null; 
 const credentials = { email: 'montenegrogerson@gmail.com', password: '123456' };
 
 describe('bpnodets Backend test controller UserController', () => {
@@ -15,7 +16,6 @@ describe('bpnodets Backend test controller UserController', () => {
             .set('Accept', 'application/json')
             .send(credentials)
             .end((err, res) => {
-                console.log(res.body)
                 res.should.have.status(200);
                 res.body.success.should.be.eql(true);
                 _TOKEN_BEARER = res.body.token;
@@ -31,12 +31,77 @@ describe('bpnodets Backend test controller UserController', () => {
             .query({ page: 0, itemsPerPage: 10 })
             .send(credentials)
             .end((err, res) => {
-                console.log(res.body)
                 res.should.have.status(200);
                 res.body.success.should.be.eql(true);
                 res.body.should.have.property('data');
                 res.body.should.be.a('object');
                 res.body.data.should.be.a('array');
+                done();
+            });
+    });
+    it('it should POST new user', (done) => {
+        const newUsers = {
+            firstName:"Juan",
+            lastName:"Canepa",
+            birth:"1980-10-17",
+            email:"jacanepa@gmail.com",
+            password:"123456"
+        };
+        chai.request(app)
+            .post('/api/v1/users')
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .set('authorization', _TOKEN_BEARER)
+            .send(newUsers)
+            .end((err, res) => {
+                res.should.have.status(201);
+                res.body.success.should.be.eql(true);
+                res.body.should.have.property('data');
+                res.body.should.be.a('object');
+                res.body.data.should.be.a('object');
+                res.body.data.should.have.property('id');
+                res.body.data.should.have.property('firstName');
+                res.body.data.should.have.property('lastName');
+                res.body.data.should.have.property('birth');
+                res.body.data.should.have.property('email');
+                res.body.data.should.have.property('password');
+                res.body.data.should.have.property('updatedAtDate');
+                res.body.data.should.have.property('group');
+                res.body.data.group.should.be.a('array');
+                res.body.data.group[0].should.have.property('id');
+                res.body.data.group[0].should.have.property('description');
+                res.body.data.group[0].should.have.property('createdAtDate');
+                res.body.data.group[0].should.have.property('updatedAtDate');
+                _ID_USER = res.body.data.id;
+                done();
+            });
+    });
+    it('it should update user', (done) => {
+        const updateFields = { "email": "jacm1980@gmail.com" }
+        chai.request(app)
+            .put(`/api/v1/users/${_ID_USER}`)
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .set('authorization', _TOKEN_BEARER)
+            .send(updateFields)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.success.should.be.eql(true);
+                res.body.should.have.property('data');
+                res.body.should.be.a('object');
+                done();
+            });
+    });
+    it('it should delete user', (done) => {
+        chai.request(app)
+            .delete(`/api/v1/users/${_ID_USER}`)
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .set('authorization', _TOKEN_BEARER)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.success.should.be.eql(true);
+                res.body.should.be.a('object');
                 done();
             });
     });
